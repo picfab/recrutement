23e415e96f47013f11c22d59a21863758cf226eb
Fabien Picard <pic.fab@hotmail.fr>
2024-05-29 15:01:00 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+
+        <h1 className='text-3xl font-bold underline text-red-600'>Let's GO</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
